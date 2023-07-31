ebfbfa7d5ac1833702667e3b453b3612d1ec7fc0
Pedro S <97417230+sousa-p@users.noreply.github.com>
2023-07-31 11:15:10 +0000

    Add cores comercio

--- FrontEndCliente/src/app/app.module.ts
+++ FrontEndCliente/src/app/app.module.ts
@@ -8,12 +8,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
-import { DateService } from './services/date.service';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, DateService],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
