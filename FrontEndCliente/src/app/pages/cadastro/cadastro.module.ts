423691a27afaa4db6088c30356b4d37321a20b38
Rômulo Gonçalves <104324780+RomuuloGoncalves@users.noreply.github.com>
2023-07-31 12:40:28 +0000

    footer

--- FrontEndCliente/src/app/pages/cadastro/cadastro.module.ts
+++ FrontEndCliente/src/app/pages/cadastro/cadastro.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { CadastroPageRoutingModule } from './cadastro-routing.module';
 
 import { CadastroPage } from './cadastro.page';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { CadastroPage } from './cadastro.page';
     IonicModule,
     CadastroPageRoutingModule
   ],
-  declarations: [CadastroPage]
+  declarations: [CadastroPage, FooterComponent]
 })
 export class CadastroPageModule {}
