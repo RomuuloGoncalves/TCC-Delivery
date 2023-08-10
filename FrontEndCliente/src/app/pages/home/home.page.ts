14daee88703c96f656b0ba59d1b1f5eb88724f74
sousa-p <p.soousa7@gmail.com>
2023-08-10 11:08:32 -0300

    Add dummy data na home

--- FrontEndCliente/src/app/pages/home/home.page.ts
+++ FrontEndCliente/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/core/interfaces/produto';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,55 @@ export class HomePage implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  produtos: Produto[] = [
+    {
+      COD_PRODUTO: 1,
+      NOME: 'Batata',
+      DESCRICAO: 'Bata muito boa tipo muito muito boa mesmo',
+      VARIACAO: {
+        COD_VARIACAO: 1,
+        NOME: 'Batata Frita',
+        VALOR_DESCONTO: 15.00,
+        VALOR_INICIAL: 19.50,
+        IMAGEM: '../../../assets/img/home-icons/garfo_faca_outline.png'
+      }
+    },
+    {
+      COD_PRODUTO: 2,
+      NOME: 'Batata',
+      DESCRICAO: 'Bata muito boa tipo muito muito boa mesmo',
+      VARIACAO: {
+        COD_VARIACAO: 2,
+        NOME: 'Batata cozida',
+        VALOR_DESCONTO: 10.00,
+        VALOR_INICIAL: 14.50,
+        IMAGEM: '../../../assets/img/home-icons/garfo_faca_outline.png'
+      }
+    },
+    {
+      COD_PRODUTO: 3,
+      NOME: 'Batata',
+      DESCRICAO: 'Bata muito boa tipo muito muito boa mesmo',
+      VARIACAO: {
+        COD_VARIACAO: 3,
+        NOME: 'Batata Doce',
+        VALOR_DESCONTO: 17.00,
+        VALOR_INICIAL: 21.00,
+        IMAGEM: '../../../assets/img/home-icons/garfo_faca_outline.png'
+      }
+    },
+    {
+      COD_PRODUTO: 1,
+      NOME: 'Batata',
+      DESCRICAO: 'Bata muito boa tipo muito muito boa mesmo',
+      VARIACAO: {
+        COD_VARIACAO: 1,
+        NOME: 'Batata Chips',
+        VALOR_DESCONTO: 20.00,
+        VALOR_INICIAL: 25.50,
+        IMAGEM: '../../../assets/img/home-icons/garfo_faca_outline.png'
+      }
+    }
+  ]
 }
