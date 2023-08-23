6309bf79e660d09f5ad27502e8d6574198ddfce6
RomuuloGoncalves <romulogoncalves13@gmail.com>
2023-08-23 19:02:03 -0300

    menu e header adm

--- FrontEndComercio/src/app/pages/home/home.module.ts
+++ FrontEndComercio/src/app/pages/home/home.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { HeaderComponent } from 'src/app/components/common/header/header.component';
 
 @NgModule({
   imports: [
@@ -16,7 +17,7 @@ import { HomePage } from './home.page';
     HomePageRoutingModule,
   ],
   declarations: [
-    HomePage
+    HomePage, HeaderComponent
   ]
 })
 export class HomePageModule {}
