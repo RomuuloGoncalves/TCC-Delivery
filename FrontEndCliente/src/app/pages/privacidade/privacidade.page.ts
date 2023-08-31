dbd6b51c073509312c1c6513bb0c6a53ac0a3ad9
Rômulo Gonçalves <104324780+RomuuloGoncalves@users.noreply.github.com>
2023-08-31 12:41:40 +0000

    criação pagina privacidade

---
+++ FrontEndCliente/src/app/pages/privacidade/privacidade.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacidade',
+  templateUrl: './privacidade.page.html',
+  styleUrls: ['./privacidade.page.scss'],
+})
+export class PrivacidadePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
