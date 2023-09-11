3ddc216eb5a0106ce6e6cdd3d38672942a4c736f
João Augusto Pereira Vieira <89317386+Jopereira2005@users.noreply.github.com>
2023-09-11 11:46:32 +0000

    add backend files

--- System/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-//HEADERS
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-header('Access-Control-Allow-Methods: POST');
