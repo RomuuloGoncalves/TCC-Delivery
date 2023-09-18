f4521bea4618115c6507e07ec3e6b8582b1f2ef1
Daniel Ferreira <125904992+DanielFedoSantos@users.noreply.github.com>
2023-09-18 12:33:16 +0000

    formulario produto

---
+++ FrontEndCliente/src/app/components/common/modal-marmita/modal-marmita.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-marmita',
+  templateUrl: './modal-marmita.component.html',
+  styleUrls: ['./modal-marmita.component.scss'],
+})
+export class ModalMarmitaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
