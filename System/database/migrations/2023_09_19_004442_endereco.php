2947dc96c6196df766b653f62372d353812d87fb
sousa-p <p.soousa7@gmail.com>
2023-09-18 22:16:30 -0300

    Add parcialmente as migrations

---
+++ System/database/migrations/2023_09_19_004442_endereco.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('cep');
+            $table->string('rua');
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('complemento');
+            $table->unsignedBigInteger('cliente_id');
+            $table->index('cliente_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ System/database/migrations/2023_09_19_004500_pedido.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('funcionario_id');
+            $table->unsignedBigInteger('cupom_id');
+            $table->date('data_pedido');
+            $table->date('data_pagamento');
+            $table->enum('status_pedido', ['pronto', 'em entrega', 'cancelado', 'em espera']);
+            $table->enum('forma_pagamento', ['crédito', 'dinheiro', 'pix', 'débito']);
+            $table->string('endereco_entrega');
+            $table->float('valor_total');
+            $table->float('valor_com_desconto');
+            $table->timestamps();
+            $table->index('cliente_id');
+            $table->index('funcionario_id');
+            $table->index('cupom_id');
+        });//
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
