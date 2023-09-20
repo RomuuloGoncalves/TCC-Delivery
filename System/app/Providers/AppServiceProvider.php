43686236db4d7f0cb6d7e4851b1f08f0331b4de5
Jopereira2005 <jopereiravieira98@gmail.com>
2023-09-19 23:42:19 -0300

    to use local db fix

--- System/app/Providers/AppServiceProvider.php
+++ System/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,16 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
+    {
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
     {
         //
     }
