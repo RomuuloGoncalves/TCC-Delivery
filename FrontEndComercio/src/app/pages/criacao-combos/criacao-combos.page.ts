4e41ff5e7c7a7f80314c66c40cf4d8e40c90a9cf
RomuuloGoncalves <romulogoncalves13@gmail.com>
2023-09-21 00:10:31 -0300

    #

--- FrontEndComercio/src/app/pages/criacao-combos/criacao-combos.page.ts
+++ FrontEndComercio/src/app/pages/criacao-combos/criacao-combos.page.ts
@@ -7,106 +7,75 @@ import { Produto } from 'src/app/core/interfaces/produto';
   styleUrls: ['./criacao-combos.page.scss'],
 })
 export class CriacaoCombosPage implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
-    console.log(this.dadoFinal)
+    console.log(this.dadoFinal);
   }
-  produtoSelecionado: string = 'Marmita';
-  grupoVariacao: string = '';
-  
-  produtoSelecionadoSelect(e: any) {
+
+  produtoSelecionado: any = [];
+  selecionado: any = []
+
+  produtoSelecionadoSelect(e: any, grupo: any) {
     this.produtoSelecionado = e.detail.value;
-    console.log(this.produtoSelecionado)
-  }
-  grupoVariacaoSelecionada(e: any) {
-    this.grupoVariacao = e.detail.value;
+    this.selecionado.push([grupo, this.produtoSelecionado])
+
+
+    console.log(this.selecionado);
   }
 
 
   marmitas: Produto[] = [
-    {
-      nome: 'Marmita de frango',
-    },
-    {
-      nome: 'Marmita de bife',
-    },
-    {
-      nome: 'Marmita de arroz',
-    },
-  ]
+    { nome: 'Marmita de frango' },
+    { nome: 'Marmita de bife' },
+    { nome: 'Marmita de arroz' },
+  ];
+
   sobremesas: Produto[] = [
-    {
-      nome: 'bolo de cenoura',
-    },
-    {
-      nome: 'bolo de chocolate',
-    },
-    {
-      nome: 'bolo de morango',
-    },
-  ]
+    { nome: 'bolo de cenoura' },
+    { nome: 'bolo de chocolate' },
+    { nome: 'bolo de morango' },
+  ];
+
   saladas: Produto[] = [
-    {
-      nome: 'Salada X',
-    },
-    {
-      nome: 'Salada y',
-    },
-  ]
+    { nome: 'Salada X' },
+    { nome: 'Salada y' },
+  ];
 
   refrigerante: Produto[] = [
-    {
-      nome: 'Coca-Cola',
-    },
-    {
-      nome: 'Pepsi',
-    },
-    {
-      nome: 'Guaraná Antarctica',
-    },
-  ]
+    { nome: 'Coca-Cola' },
+    { nome: 'Pepsi' },
+    { nome: 'Guaraná Antarctica' },
+  ];
+
   suco: Produto[] = [
-    {
-      nome: 'Suco de Laranja',
-    },
-    {
-      nome: 'Suco de Maçã',
-    },
-    {
-      nome: 'Suco de Uva',
-    },
-  ]
+    { nome: 'Suco de Laranja' },
+    { nome: 'Suco de Maçã' },
+    { nome: 'Suco de Uva' },
+  ];
+
   cha: Produto[] = [
-    {
-      nome: 'Chá de Camomila',
-    },
-    {
-      nome: 'Chá Verde',
-    },
-    {
-      nome: 'Chá de Hortelã',
-    },
-  ]
+    { nome: 'Chá de Camomila' },
+    { nome: 'Chá Verde' },
+    { nome: 'Chá de Hortelã' },
+  ];
 
   comidas = {
-    marmitas: this.marmitas, 
-    sobremeas: this.sobremesas, 
-    saladas: this.saladas
-  }
-
-  bebidas = {
-    refigerante: this.refrigerante, 
-    suco: this.suco, 
-    cha: this.cha
+    marmitas: this.marmitas,
+    sobremesas: this.sobremesas,
+    saladas: this.saladas,
   };
 
+  bebidas = {
+    refrigerante: this.refrigerante,
+    suco: this.suco,
+    cha: this.cha,
+  };
 
   dados = {
     comidas: Object.entries(this.comidas),
     bebidas: Object.entries(this.bebidas),
-  }
+  };
 
   dadoFinal = Object.entries(this.dados);
 }
