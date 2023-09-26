c1ee8062f87cbe0ec1672ba3c45a7931f8c806ff
Pedro S. <97417230+sousa-p@users.noreply.github.com>
2023-09-26 13:49:49 +0000

    Feature cadastro

--- FrontEndCliente/src/app/pages/cadastro/cadastro.page.ts
+++ FrontEndCliente/src/app/pages/cadastro/cadastro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ClienteService } from 'src/app/core/services/cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,11 +9,30 @@ import { NgForm } from '@angular/forms';
 })
 export class CadastroPage implements OnInit {
 
-  constructor(private ) { }
+  constructor(private Cliente: ClienteService) { }
 
   @ViewChild('cadastroForm') private cadastoForm!: NgForm;
 
   ngOnInit() {
   }
 
+  public cadastrar() {
+    const valores = this.cadastoForm.form.value;
+    const cliente = {
+      'nome': valores.nome,
+      'email': valores.email,
+      'password': valores.password,
+      'telefone': valores.telefone,
+    }
+
+    this.Cliente.cadastro(cliente).subscribe(
+      (response: any) => {
+        console.log(response);
+      }, 
+
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
 }
