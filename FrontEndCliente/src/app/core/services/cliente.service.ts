81f3933f14226d0a7ef0642c603f9e6315fd0161
Daniel Ferreira <125904992+DanielFedoSantos@users.noreply.github.com>
2023-09-26 13:41:53 +0000

    service cliente

---
+++ FrontEndCliente/src/app/core/services/cliente.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor() { }
+
+  url: string = "https://opulent-enigma-566x5577w6rh4rg7-8000.app.github.dev/cliente/login"
+
+  
+
+}
