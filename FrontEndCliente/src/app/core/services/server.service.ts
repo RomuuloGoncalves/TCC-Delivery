838680bc5a27279c5805b99beeb938615d526ad9
Pedro S. <97417230+sousa-p@users.noreply.github.com>
2023-09-26 13:33:40 +0000

    Feature server service

--- FrontEndCliente/src/app/core/services/server.service.ts
+++ FrontEndCliente/src/app/core/services/server.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServerService {
+  constructor(private http: HttpClient, private Cookie: CookieService) {}
 
-  constructor() { }
+  private url: string = 'http://localhost/BoituLab/System/';
+
+  public post(path: string, data: any): Observable<any> {
+    const token = this.Cookie.get('token');
+    if (token !== '') {
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      return this.http.post(this.url, JSON.stringify(data), { headers });
+    }
+    return this.http.post(this.url, JSON.stringify(data));
+  }
 }
