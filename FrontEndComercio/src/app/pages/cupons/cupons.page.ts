7c62197c4d259e43a28dc21bf43d8fc53c692a54
Rômulo Gonçalves <104324780+RomuuloGoncalves@users.noreply.github.com>
2023-09-30 12:11:34 +0000

    #

--- FrontEndComercio/src/app/pages/cupons/cupons.page.ts
+++ FrontEndComercio/src/app/pages/cupons/cupons.page.ts
@@ -10,58 +10,67 @@ export class CuponsPage implements OnInit {
 
   constructor() { }
 
-  public cupons: Cupom[] = [
-    {
-      id_cupom: 1,
-      nome: 'Feijuca',
-      quantidade: 45,
-      porcentagem_desconto: 15,
-      data_validade: '2023-12-12',
-      status: true
-    },
-    {
-      id_cupom: 1,
-      nome: 'Feijuca',
-      quantidade: 45,
-      porcentagem_desconto: 15,
-      data_validade: '2023-12-12',
-      status: true
-    },
-    {
-      id_cupom: 1,
-      nome: 'Feijuca',
-      quantidade: 45,
-      porcentagem_desconto: 15,
-      data_validade: '2023-12-12',
-      status: true
-    },
-    {
-      id_cupom: 1,
-      nome: 'Feijuca',
-      quantidade: 45,
-      porcentagem_desconto: 15,
-      data_validade: '2023-12-12',
-      status: true
-    },
-    {
-      id_cupom: 1,
-      nome: 'Feijuca',
-      quantidade: 45,
-      porcentagem_desconto: 15,
-      data_validade: '2023-12-12',
-      status: true
-    },
-    {
-      id_cupom: 1,
-      nome: 'Feijuca',
-      quantidade: 45,
-      porcentagem_desconto: 15,
-      data_validade: '2023-12-12',
-      status: false
-    },
-  ];
+  cupons!: Cupom[]
+  url = 'http://127.0.0.1:8000/cupom/listar'
 
   ngOnInit() {
     console.log(this.cupons)
   }
+
+  recuperarCupons() {
+    fetch(this.url)
+      .then(response => console.log(response))
+
+  }
+  // public cupons: Cupom[] = [
+  //   {
+  //     id_cupom: 1,
+  //     nome: 'Feijuca',
+  //     quantidade: 45,
+  //     porcentagem_desconto: 15,
+  //     data_validade: '2023-12-12',
+  //     status: true
+  //   },
+  //   {
+  //     id_cupom: 1,
+  //     nome: 'Feijuca',
+  //     quantidade: 45,
+  //     porcentagem_desconto: 15,
+  //     data_validade: '2023-12-12',
+  //     status: true
+  //   },
+  //   {
+  //     id_cupom: 1,
+  //     nome: 'Feijuca',
+  //     quantidade: 45,
+  //     porcentagem_desconto: 15,
+  //     data_validade: '2023-12-12',
+  //     status: true
+  //   },
+  //   {
+  //     id_cupom: 1,
+  //     nome: 'Feijuca',
+  //     quantidade: 45,
+  //     porcentagem_desconto: 15,
+  //     data_validade: '2023-12-12',
+  //     status: true
+  //   },
+  //   {
+  //     id_cupom: 1,
+  //     nome: 'Feijuca',
+  //     quantidade: 45,
+  //     porcentagem_desconto: 15,
+  //     data_validade: '2023-12-12',
+  //     status: true
+  //   },
+  //   {
+  //     id_cupom: 1,
+  //     nome: 'Feijuca',
+  //     quantidade: 45,
+  //     porcentagem_desconto: 15,
+  //     data_validade: '2023-12-12',
+  //     status: false
+  //   },
+  // ];
+
 }
