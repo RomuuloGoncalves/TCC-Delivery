cab48c9c618c5d1a19424e3cab915da0553f421a
João Augusto Pereira Vieira <89317386+Jopereira2005@users.noreply.github.com>
2023-09-30 16:57:11 +0000

    .

--- System/app/Http/Controllers/EnderecoController.php
+++ System/app/Http/Controllers/EnderecoController.php
@@ -1,1 +1,41 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use App\Models\Endereco;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class EnderecoController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $regras = [
+            'nome' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:Clientes'],
+            'password' => ['required', 'string', 'max:255', 'min:8'],
+            'telefone' => ['required', 'string', 'size:11', 'unique:Clientes'],
+        ];
+
+        $validacao = Validator::make($request->all(), $regras);
+
+        if ($validacao->fails())
+            return response()->json($validacao->errors(), 422);
+
+        $cliente = Endereco::create([
+
+        ]);
+
+        return response()->json($cliente, 201);
+    }
+}
