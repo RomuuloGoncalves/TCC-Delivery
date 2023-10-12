95ac962c5d77ecab0df9b1d1dd03b263e0072d3d
RomuuloGoncalves <romulogoncalves13@gmail.com>
2023-10-12 13:35:31 -0300

    gráficos do financeito

--- FrontEndComercio/src/app/pages/admin/taxa-cancelamento/taxa-cancelamento.page.ts
+++ FrontEndComercio/src/app/pages/admin/taxa-cancelamento/taxa-cancelamento.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-taxa-cancelamento',
@@ -9,7 +11,38 @@ export class TaxaCancelamentoPage implements OnInit {
 
   constructor() { }
 
+
   ngOnInit() {
+    this.gerarGrafico()
+  }
+  
+  gerarGrafico() {
+    var myChart = new Chart("myChart", {
+      type: 'pie',
+      data: {
+        labels: ['Cancelado', 'Entregue'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   }
 
 }
