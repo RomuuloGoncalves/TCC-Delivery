38cdcdd01666b6fd32249ab774cc9246cd67e304
Daniel Ferreira <125904992+DanielFedoSantos@users.noreply.github.com>
2023-10-23 21:32:57 +0000

    # com erros

--- FrontEndCliente/src/app/pages/montagem-marmita/montagem-marmita.page.ts
+++ FrontEndCliente/src/app/pages/montagem-marmita/montagem-marmita.page.ts
@@ -4,6 +4,8 @@ import { GrupoVariacoes } from 'src/app/core/interfaces/grupo-variacoes';
 
 import Swiper from 'swiper';
 import { PassarMarmitaService } from 'src/app/core/services/passar-marmita.service';
+import { ProdutoService } from 'src/app/core/services/produto.service';
+import { Produto } from 'src/app/core/interfaces/produto';
 
 @Component({
   selector: 'app-montagem-marmita',
@@ -15,168 +17,38 @@ export class MontagemMarmitaPage implements OnInit {
   swiperRef: ElementRef | undefined;
   swiper?: Swiper;
 
-  breakpoints = {
-    576: {
-      slidesPerView: 1,
-      spaceBetween: 15,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-  };
-
   marmita: GrupoVariacoes[] = [];
 
   precoMarmita: number = 0.0;
   qtddMarmita: number = 1;
   total: number = 0.0;
 
-  ingredientes: GrupoVariacoes[] = [
-    {
-      tipo: 'Arroz',
-      id_produto: 2,
-      id_grupo_variacoes: 3,
+  ingredientes!: Produto
 
-      variacoes: [
-        {
-          id_variacao: 1,
-          nome: 'Branco',
-          descricao: 'Soltinho!',
-          valor_inicial: 7.0,
-          valor_final: 5.0,
-          selecionado: false
-        },
-        {
-          id_variacao: 2,
-          nome: 'Integral',
-          descricao: 'Para ficar fitness',
-          valor_inicial: 8.0,
-          valor_final: 8.0,
-          selecionado: false
-        },
-        {
-          id_variacao: 3,
-          nome: 'Grego',
-          descricao: 'À moda da casa',
-          valor_inicial: 6.0,
-          valor_final: 6.0,
-          selecionado: false
-        },
-      ],
-    },
-    {
-      tipo: 'Feijão',
-      id_produto: 5,
-      id_grupo_variacoes: 4,
-
-      variacoes: [
-        {
-          id_variacao: 5,
-          nome: 'Tutu',
-          descricao: 'Especialidade da casa',
-          valor_inicial: 7,
-          valor_final: 6,
-          selecionado: false
-        },
-        {
-          id_variacao: 6,
-          nome: 'Branco',
-          descricao: 'Feijão branco e quentinho!',
-          valor_inicial: 10,
-          valor_final: 8.5,
-          selecionado: false
-        },
-        {
-          id_variacao: 7,
-          nome: 'Preto',
-          descricao: 'Feijão preto e quentinho!',
-          valor_inicial: 11.0,
-          valor_final: 9.99,
-          selecionado: false
-        },
-      ],
-    },
-    {
-      id_grupo_variacoes: 1,
-      tipo: 'Acompanhamentos',
-      qtdd_max: 1,
-
-      variacoes: [
-        {
-          id_variacao: 4,
-          nome: 'mandioquinha',
-          descricao: 'mandioca frita',
-          valor_inicial: 8.0,
-          valor_final: 8.0,
-          selecionado: false
-        },
-        {
-          id_variacao: 5,
-          nome: 'Filé',
-          descricao: 'Filé zica',
-          valor_inicial: 11.0,
-          valor_final: 9.49,
-          selecionado: false
-        },
-        {
-          id_variacao: 6,
-          nome: 'Filé Acebolado',
-          descricao: 'Filé acebolado zika',
-          valor_inicial: 11.0,
-          valor_final: 9.49,
-          selecionado: false
-        },
-        {
-          id_variacao: 7,
-          nome: 'Patinho',
-          descricao: 'Quack Quack zica',
-          valor_inicial: 10,
-          valor_final: 9.49,
-          selecionado: false
-        },
-      ],
-    },
-    {
-      tipo: 'Salada',
-      id_produto: 2,
-      id_grupo_variacoes: 3,
-      qtdd_max: 2,
-
-      variacoes: [
-        {
-          id_variacao: 1,
-          nome: 'Alface',
-          descricao: 'Verdinho e maravilhoso!',
-          valor_inicial: 5.0,
-          valor_final: 3.0,
-          selecionado: false
-        },
-        {
-          id_variacao: 2,
-          nome: 'Tomate',
-          descricao: 'Vermelhinho e gostoso!',
-          valor_inicial: 8.0,
-          valor_final: 8.0,
-          selecionado: false
-        },
-        {
-          id_variacao: 3,
-          nome: 'Rúcula',
-          descricao: 'Verdona e gostosa!',
-          valor_inicial: 6.0,
-          valor_final: 6.0,
-          selecionado: false
-        },
-      ],
-    },
-  ];
-
-  constructor(private passarMarmita: PassarMarmitaService) {}
+  constructor(private passarMarmita: PassarMarmitaService, private produtoService: ProdutoService) {}
 
   ngOnInit() {
     this.atualizarMarmita();
     console.log(this.passarMarmita.resgatarMarmita())
+    this.carregarMarmita()
+  }
+
+
+  marmitaPersonalizavel!: Produto
+  loading: boolean = true
+
+  private carregarMarmita() {
+    // _______________________ trocar por 6 (SEIS) depois __________________________
+    this.produtoService.pegarProduto(5).subscribe(
+      (response: Produto) => {
+        this.ingredientes = response;
+        this.loading = false
+        console.log("marmita",this.marmitaPersonalizavel)
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
   }
 
   proximoCard() {
@@ -188,7 +60,8 @@ export class MontagemMarmitaPage implements OnInit {
   }
 
   selecionarIngrediente(idIngrediente: number, idVariacao: number, event: any) {
-    this.ingredientes[idIngrediente].variacoes![idVariacao].selecionado =
+    console.log(event.detail.checked)
+    this.ingredientes.grupo_variacao?[idIngrediente].variacao[idVariacao].selecionado =
       event.detail.checked;
     this.atualizarMarmita();
     // console.log(this.ingredientes)
@@ -224,4 +97,16 @@ export class MontagemMarmitaPage implements OnInit {
   calcTotal() {
     this.total = this.precoMarmita * this.qtddMarmita;
   }
+
+  breakpoints = {
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  };
+
 }
--- FrontEndComercio/src/app/core/services/server.service.ts
+++ FrontEndComercio/src/app/core/services/server.service.ts
@@ -9,9 +9,10 @@ import { CookieService } from 'ngx-cookie-service';
 export class ServerService {
   constructor(private http: HttpClient, private Cookie: CookieService) {}
 
-  private url: string = 'http://127.0.0.1:8000';
+  // private url: string = 'http://127.0.0.1:8000';
   // private url: string = 'https://cuddly-funicular-jpv6ggxg59xc5vvg-8000.app.github.dev';
   // private url: string = 'https://cautious-meme-w6w999p5693gpgq-8000.app.github.dev/';
+  private url: string = 'https://opulent-enigma-566x5577w6rh4rg7-8000.app.github.dev';
 
 
   public post(path: string, data: any): Observable<any> {
