b067abb15cbcf2c9cddac5f498333e931b6435f6
Jopereira2005 <jopereiravieira98@gmail.com>
2023-11-05 21:53:33 -0300

    feature Pedido Produto

--- FrontEndCliente/src/app/core/services/pedido-produto.service.ts
+++ FrontEndCliente/src/app/core/services/pedido-produto.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { ServerService } from './server.service';
 import { Pedido } from '../interfaces/pedido';
 import { PedidoProduto } from '../interfaces/pedido-produto';
-import { PedidoProdutoGrupoVariacoes } from '../interfaces/ped-prod-grupo-var';
 import { VariacaoSelecionada } from '../interfaces/variacao-selecionada';
 
 
@@ -13,11 +12,33 @@ export class PedidoProdutoService {
 
   constructor(private Server: ServerService) { }
 
-  public criar(ped_prod_grupo_var: PedidoProdutoGrupoVariacoes) {
-    return this.Server.post('ped-prod-grupo-var/cadastrar', ped_prod_grupo_var);
+  public criar(pedido_produto: PedidoProduto) {
+    return this.Server.post('pedido-produto/cadastrar', pedido_produto);
   }
 
-  public editar (ped_prod_grupo_var: PedidoProdutoGrupoVariacoes) {
-    return this.Server.post('ped-prod-grupo-var/cadastrar', ped_prod_grupo_var);
+  public info(id: Number) {
+    return this.Server.get(`pedido-produto/${id}`);
+  }
+
+  public editar(pedido_produto: PedidoProduto) {
+    return this.Server.put('pedido-produto/editar', pedido_produto);
+  }
+
+  public excluir(id: Number) {
+    return this.Server.delete(`pedido-produto/excluir/${id}`);
+  }
+
+  // Variação Selecionada
+
+  public cadastrarVariacaoSelecionada(variacao_selecionada: VariacaoSelecionada) {
+    return this.Server.post('pedido-produto/variacao-selecionada/cadastrar', variacao_selecionada);
+  }
+
+  public editarVariacaoSelecionada(variacao_selecionada: VariacaoSelecionada) {
+    return this.Server.put('pedido-produto/variacao-selecionada/editar', variacao_selecionada);
+  }
+
+  public excluirVariacaoSelecionada(id: Number) {
+    return this.Server.delete(`pedido-produto/variacao-selecionada/excluir/${id}`);
   }
 }
