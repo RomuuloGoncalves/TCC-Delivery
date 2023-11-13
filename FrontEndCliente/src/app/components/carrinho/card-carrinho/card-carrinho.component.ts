796b78a52c381fb5c007aaf3acbac70dbf796348
Rômulo Gonçalves <104324780+RomuuloGoncalves@users.noreply.github.com>
2023-11-13 12:42:22 +0000

    #

--- FrontEndCliente/src/app/components/carrinho/card-carrinho/card-carrinho.component.ts
+++ FrontEndCliente/src/app/components/carrinho/card-carrinho/card-carrinho.component.ts
@@ -14,7 +14,15 @@ export class CardCarrinhoComponent implements OnInit {
   @Output() calc: EventEmitter<any> = new EventEmitter();
   @Output() remover: EventEmitter<PedidoProduto> = new EventEmitter();
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.organizarImagens(this.pedidoProduto)
+  }
+
+  organizarImagens(produto:any){
+    console.log(produto)
+    produto.produto.imagem = (produto.produto.imagem) ? `https://cuddly-funicular-jpv6ggxg59xc5vvg-8000.app.github.dev/uploads/${produto.produto.imagem}` : '../../../assets/imgs/default/garfo_faca_outline.png';
+    console.log(produto.produto.imagem)
+  }
 
   removerProduto() {
     this.remover.emit(this.pedidoProduto);
