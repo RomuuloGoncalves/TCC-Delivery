c6035cd4577f221310065f44c7a1fba02d299fbe
RomuuloGoncalves <romulogoncalves13@gmail.com>
2023-11-13 20:43:12 -0300

    variacoes

--- System/database/seeders/GrupoVariacaoSeeder.php
+++ System/database/seeders/GrupoVariacaoSeeder.php
@@ -11,41 +11,13 @@ class GrupoVariacaoSeeder extends Seeder
 {
     public function run()
     {
-        // $marmitas_bebidas = DB::table('Produtos')->where('cod_categoria', 1)->orWhere('cod_categoria', 2)->get()->pluck('id')->toArray();
+        $marmitex_do_dia = DB::table('Produtos')->where('cod_categoria', 2)->get()->pluck('id')->toArray();
 
-        // DB::table('Grupo_variacoes')->insert([
-        //     'tipo' => 'Tamanho',
-        //     'cod_produto' => 1,
-        // ]);
-
-        // DB::table('Grupo_variacoes')->insert([
-        //     'tipo' => 'Arroz',
-        //     'cod_produto' => 1,
-        // ]);
-
-        // DB::table('Grupo_variacoes')->insert([
-        //     'tipo' => 'Feijão',
-        //     'quantidade_variacoes_min' => 1,
-        //     'cod_produto' => 1,
-        // ]);
-
-        // DB::table('Grupo_variacoes')->insert([
-        //     'tipo' => 'Guarnição',
-        //     'quantidade_variacoes_max' => 2,
-        //     'cod_produto' => 1,
-        // ]);
-
-        // DB::table('Grupo_variacoes')->insert([
-        //     'tipo' => 'Acompanhamento',
-        //     'quantidade_variacoes_max' => 1,
-        //     'cod_produto' => 1,
-        // ]);
-
-        // foreach($marmitas_bebidas as $id) {
-        //     DB::table('Grupo_variacoes')->insert([
-        //         'tipo' => 'Tamanho',
-        //         'cod_produto' => $id,
-        //     ]);
-        // }
+        foreach($marmitex_do_dia as $id) {
+            DB::table('Grupo_variacoes')->insert([
+                'tipo' => 'Tamanho',
+                'cod_produto' => $id,
+            ]);
+        }
     }
 }
--- System/database/seeders/VariacaoSeeder.php
+++ System/database/seeders/VariacaoSeeder.php
@@ -11,136 +11,36 @@ class VariacaoSeeder extends Seeder
 {
     public function run()
     {
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'P',
-    //         'valor' => 20.00,
-    //         'cod_grupo_variacoes' => 1,
-    //     ]);
+        for ($i = 1; $i < 10; $i++) {
+            DB::table('Variacoes')->insert([
+                'nome' => 'P',
+                'valor' => 26.99,
+                'cod_grupo_variacoes' => $i,
+            ]);
+        }
 
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'M',
-    //         'valor' => 25.00,
-    //         'cod_grupo_variacoes' => 1,
-    //     ]);
+        DB::table('Variacoes')->insert([
+            'nome' => 'P',
+            'valor' => 15.99,
+            'cod_grupo_variacoes' => 10,
+        ]);
 
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'G',
-    //         'valor' => 30.00,
-    //         'cod_grupo_variacoes' => 1,
-    //     ]);
+        DB::table('Variacoes')->insert([
+            'nome' => 'P',
+            'valor' => 47.98,
+            'cod_grupo_variacoes' => 11,
+        ]);
 
-    //     //  Arroz
+        DB::table('Variacoes')->insert([
+            'nome' => 'P',
+            'valor' => 47.98,
+            'cod_grupo_variacoes' => 12,
+        ]);
 
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Arroz Branco',
-    //         'valor' => 0.00,
-    //         'cod_grupo_variacoes' => 2,
-    //     ]);
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Arroz Integral',
-    //         'valor' => 5.00,
-    //         'cod_grupo_variacoes' => 2,
-    //     ]);
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Arroz à Grega',
-    //         'valor' => 10.00,
-    //         'cod_grupo_variacoes' => 2,
-    //     ]);
-
-    //     // Feijão
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Feijão Carioca',
-    //         'valor' => 0.00,
-    //         'cod_grupo_variacoes' => 3,
-    //     ]);
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Feijão Branco',
-    //         'valor' => 5.00,
-    //         'cod_grupo_variacoes' => 3,
-    //     ]);
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Tutu de Feijão',
-    //         'valor' => 5.00,
-    //         'cod_grupo_variacoes' => 3,
-    //     ]);
-
-    //     // Guarnição
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Filé de Frango',
-    //         'valor' => 0.00,
-    //         'cod_grupo_variacoes' => 4,
-    //     ]);
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Bife',
-    //         'valor' => 0.00,
-    //         'cod_grupo_variacoes' => 4,
-    //     ]);
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Picanha',
-    //         'valor' => 10.00,
-    //         'cod_grupo_variacoes' => 4,
-    //     ]);
-
-    //     // Acompanhamento
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Farofa',
-    //         'valor' => 5.00,
-    //         'cod_grupo_variacoes' => 5,
-    //     ]);
-
-    //     DB::table('Variacoes')->insert([
-    //         'nome' => 'Maionese',
-    //         'valor' => 5.00,
-    //         'cod_grupo_variacoes' => 5,
-    //     ]);
-
-    //     for ($i=6 ;$i <= 15; $i++) {
-    //         if($i < 10) {
-    //             DB::table('Variacoes')->insert([
-    //                 'nome' => 'P',
-    //                 'valor' => 20.00,
-    //                 'cod_grupo_variacoes' => $i,
-    //             ]);
-
-    //             DB::table('Variacoes')->insert([
-    //                 'nome' => 'M',
-    //                 'valor' => 25.00,
-    //                 'cod_grupo_variacoes' => $i,
-    //             ]);
-
-    //             DB::table('Variacoes')->insert([
-    //                 'nome' => 'G',
-    //                 'valor' => 30.00,
-    //                 'cod_grupo_variacoes' => $i,
-    //             ]);
-    //         } else {
-    //             DB::table('Variacoes')->insert([
-    //                 'nome' => '300ml',
-    //                 'valor' => 5.00,
-    //                 'cod_grupo_variacoes' => $i,
-    //             ]);
-
-    //             DB::table('Variacoes')->insert([
-    //                 'nome' => '400ml',
-    //                 'valor' => 7.50,
-    //                 'cod_grupo_variacoes' => $i,
-    //             ]);
-
-    //             DB::table('Variacoes')->insert([
-    //                 'nome' => '500ml',
-    //                 'valor' => 10.00,
-    //                 'cod_grupo_variacoes' => $i,
-    //             ]);
-    //         }
-    //     }
+        DB::table('Variacoes')->insert([
+            'nome' => 'P',
+            'valor' => 13.00,
+            'cod_grupo_variacoes' => 13,
+        ]);
     }
 }
