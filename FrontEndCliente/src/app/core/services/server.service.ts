85130b68e9b71a193da82c4fd9e61d1e96feb16c
Rômulo Gonçalves <104324780+RomuuloGoncalves@users.noreply.github.com>
2023-11-14 13:26:38 +0000

    #

--- FrontEndCliente/src/app/core/services/server.service.ts
+++ FrontEndCliente/src/app/core/services/server.service.ts
@@ -9,9 +9,9 @@ import { CookieService } from 'ngx-cookie-service';
 export class ServerService {
   constructor(private http: HttpClient, private Cookie: CookieService) {}
 
-  private url: string = 'http://127.0.0.1:8000';
+  // private url: string = 'http://127.0.0.1:8000';
   // private url: string = 'https://cautious-meme-w6w999p5693gpgq-8000.app.github.dev/';
-  // private url: string = 'https://cuddly-funicular-jpv6ggxg59xc5vvg-8000.app.github.dev';
+  private url: string = 'https://cuddly-funicular-jpv6ggxg59xc5vvg-8000.app.github.dev';
 
   public post(path: string, data: any | null, url: string = this.url): Observable<any> {
     const headers = new HttpHeaders({
