c573751d6a2f679d9331d45dbe0a9d59ccb4f57c
RomuuloGoncalves <romulogoncalves13@gmail.com>
2023-11-16 18:43:27 -0300

    #

--- FrontEndComercio/src/app/core/services/server.service.ts
+++ FrontEndComercio/src/app/core/services/server.service.ts
@@ -9,10 +9,10 @@ import { CookieService } from 'ngx-cookie-service';
 export class ServerService {
   constructor(private http: HttpClient, private Cookie: CookieService) {}
 
-  private url: string = 'https://super-cod-9vvjwwggp66hpq6j-8000.app.github.dev';
+  // private url: string = 'https://super-cod-9vvjwwggp66hpq6j-8000.app.github.dev';
   // private url: string = 'https://cuddly-funicular-jpv6ggxg59xc5vvg-8000.app.github.dev';
   // private url: string = 'https://bug-free-meme-jw7977wx94gc9qr-8000.app.github.dev';
-  // private url: string = 'http://127.0.0.1:8000';
+  private url: string = 'http://127.0.0.1:8000';
 
 
   public post(path: string, data: any): Observable<any> {
